6e44b232964225ace87521605cb808fe7080b799
Petryck William <petryck@i9devgroup.com>
2025-04-29 22:04:03 -0300

    .
    .

---
+++ dist/sound/index.ts
@@ -0,0 +1,21 @@
+import death from "./character_death.mp3";
+import sword from "./sword.mp3";
+import coins from "./coins.mp3";
+import hit from "./cinematic-hit.mp3";
+import eat from "./eat.mp3";
+import jungle_night from "./jungle_night.mp3";
+import ocean from "./ocean.mp3";
+import game_over from "./game_over.mp3";
+import seagull from "./seagull.mp3";
+
+export {
+  jungle_night,
+  seagull,
+  game_over,
+  coins,
+  death,
+  eat,
+  hit,
+  sword,
+  ocean,
+};
